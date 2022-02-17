fb019ff80f9f9291e365bb7e9d199a2f1e4998de
David Ogar <owogogahhero@outlook.com>
2022-02-17 17:20:22 +0100

    starting project

---
+++ src/main/java/com/TheTrueHooha/Ivy/IvyApplication.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Ivy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheTrueHooha/Ivy/IvyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Ivy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

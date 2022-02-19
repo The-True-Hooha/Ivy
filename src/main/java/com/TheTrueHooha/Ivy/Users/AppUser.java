0376a49d5e45fd61928489d017107ae71ca2b9da
David Ogar <owogogahhero@outlook.com>
2022-02-19 17:34:29 +0100

    completed the app user parameters

--- src/main/java/com/TheTrueHooha/Ivy/Users/AppUser.java
+++ src/main/java/com/TheTrueHooha/Ivy/Users/AppUser.java
@@ -1,15 +1,38 @@
 package com.TheTrueHooha.Ivy.Users;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
+
+//lombok to create getters and setters
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 
 //user details keyword deals with the spring security for user config settings
 public class AppUser implements UserDetails {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue (
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     //defined properties for the users
     private Long id;
     private String name;
@@ -17,9 +40,31 @@ public class AppUser implements UserDetails {
     private String email;
     private String password;
     private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
+
+
+    //constructor for the user properties
+    public AppUser(String name,
+                   String username,
+                   String email,
+                   String password,
+                   String phoneNumber,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {

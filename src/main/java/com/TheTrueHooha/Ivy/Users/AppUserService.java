ba9ad19edf219bfbd59e38f56d46d6a46c4458a5
David Ogar <owogogahhero@outlook.com>
2022-02-20 08:57:38 +0100

    updated progress

--- src/main/java/com/TheTrueHooha/Ivy/Users/AppUserService.java
+++ src/main/java/com/TheTrueHooha/Ivy/Users/AppUserService.java
@@ -1,12 +1,26 @@
 package com.TheTrueHooha.Ivy.Users;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND = "user with email %s not found";
+    private final static String PHONE_NOT_FOUND = "user with phone %s not found";
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(()
+                -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
     }
+
+
+
 }
---
+++ src/main/java/com/TheTrueHooha/Ivy/Users/UserRepository.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Ivy.Users;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional (readOnly = true)
+public interface UserRepository {
+    Optional <AppUser> findByEmail  (String email);
+    Optional <AppUser> findByPhoneNumber (String phoneNumber);
+}

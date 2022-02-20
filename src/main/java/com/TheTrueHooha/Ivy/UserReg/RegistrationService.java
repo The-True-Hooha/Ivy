9d0484e7ecabe8aab1d14fcd4973839be1837a1d
David Ogar <owogogahhero@outlook.com>
2022-02-20 15:56:23 +0100

    secured all the endpoints

--- src/main/java/com/TheTrueHooha/Ivy/UserReg/RegistrationService.java
+++ src/main/java/com/TheTrueHooha/Ivy/UserReg/RegistrationService.java
@@ -1,5 +1,9 @@
 package com.TheTrueHooha.Ivy.UserReg;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "working";
